b886f369265f1c8a7014fa866e8c324ee49cea86
myk245 <myk245@nyu.edu>
2020-12-03 16:28:37 -0500

    edits to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+   def index
+      movies = Movie.all 
+      render json: movies
+   end 
+
+   def show
+      movie = Movie.find(params[:id])
+      render json: movie 
+   end 
+
+   def update 
+      movie = Movie.find(params[:id])
+      movie.update(movie_params)
+      render json: movie
+   end 
+
+   private 
+   def movie_params
+      params.require(:movie).permit(:title, :movieId, :likes, :dislikes)
+   end 
 end
