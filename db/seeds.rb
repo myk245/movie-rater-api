38139226c0b200374061d8de1aa5b04606b2160a
myk245 <myk245@nyu.edu>
2020-12-04 15:07:46 -0500

    api versioning and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+m1 = Movie.create(title: "Cinderella", movieId: "tt1661199", likes: 100, dislikes: 23)
+
+m2 = Movie.create(title: "She's the Man", movieId: "tt0454945", likes: 500, dislikes: 1)
+
+m3 = Movie.create(title: "Alien", movieId: "tt0078748", likes: 96, dislikes: 3)
+
+m4 = Movie.create(title: "The Witch", movieId: "tt4263482", likes: 666, dislikes: 4)
+
+m5 = Movie.create(title: "The Lighthouse", movieId: "tt7984734", likes: 23, dislikes: 23)
